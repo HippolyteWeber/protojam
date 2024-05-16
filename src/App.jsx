ae9ff6ebf968c1336dbe632bb719814f7b9e2251
RiZainou <m.toun976@gmail.com>
2024-05-16 16:01:37 +0200

    create card details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import "./App.css";
-<<<<<<< HEAD
+
 import Header from "./components/Header.jsx";
 
-function App() {
-  return (
-    <div>
-      <Header />
-    </div>
-=======
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Header />
       <Navbar />
       <Footer />
     </>
->>>>>>> e59536acc367262acaa597332ba1091249525abc
   );
 }
 
