ecde2d4a2007c9768714fc0d0b7239eebed50ff4
HippolyteWeber <hippolyte.weber@gmail.com>
2024-05-16 18:43:17 +0200

    fix: about page

--- src/pages/AboutList.jsx
+++ src/pages/AboutList.jsx
@@ -1,15 +1,20 @@
 import AboutCard from "./AboutCard";
 import teamMembers from "../data/teamMembers";
 import "../styles/About.scss";
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 function AboutList() {
   return (
-    <div className="about-section">
-      <div className="team-section">
-        {teamMembers.map((teamMember) => (
-          <AboutCard key={teamMember.id} teamMember={teamMember} />
-        ))}
+    <div className="about-background">
+      <Navbar />
+      <div className="about-section">
+        <div className="team-section">
+          {teamMembers.map((teamMember) => (
+            <AboutCard key={teamMember.id} teamMember={teamMember} />
+          ))}
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
