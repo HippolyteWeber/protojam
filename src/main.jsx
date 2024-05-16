0404373c07c00ad126ab41ea8afc99552ebdd5b3
Mohamed Jebri <159800102+memedjjebri@users.noreply.github.com>
2024-05-16 16:47:52 +0200

    feat: adding About page

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App';
-import './index.css';
-/* import AboutList from "./pages/AboutList"; */
+import App from "./App";
+import "./index.css";
+import AboutList from "./pages/AboutList";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-/*   {
+  {
     path: "/about",
     element: <AboutList />,
-  }, */
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
