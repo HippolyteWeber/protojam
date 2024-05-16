edf808723afaf41c31ee698041d4dde7297ab89f
AurelienChetot <aurelien.chetot@gmail.com>
2024-05-16 16:40:39 +0200

    center NavBar and style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,26 @@
 export default function Navbar() {
   return (
     <div>
-      <a className="btn btn-ghost text-xl">Code Nature</a>
-      <div className="navbar-center hidden lg:flex justify-evenly">
-        <ul className="menu menu-horizontal px-1 gap-48">
+      <div className="navbar-center lg:flex justify-evenly max-[1023px]:items-center flex ">
+        <ul className="menu menu-horizontal px-1 gap-32 max-[1023px]:gap-0 ">
           <li>
-            <a>Parcs</a>
+            <a className="text-white">Parcs</a>
           </li>
           <li>
             <details>
-              <summary>Autre</summary>
+              <summary className="text-white">Autre</summary>
               <ul className="p-2">
                 <li>
-                  <a>Fleurs</a>
+                  <a className="text-white">Fleurs</a>
                 </li>
                 <li>
-                  <a>Plein Air</a>
+                  <a className="text-white">Plein Air</a>
                 </li>
               </ul>
             </details>
           </li>
           <li>
-            <a>A propos</a>
+            <a className="text-white">A propos</a>
           </li>
         </ul>
       </div>
