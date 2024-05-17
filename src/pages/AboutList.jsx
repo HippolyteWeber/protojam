4430e28f4286e5f97075dfc40931d135adb2f5ba
RiZainou <m.toun976@gmail.com>
2024-05-17 10:27:37 +0200

    create home page fix route

--- src/pages/AboutList.jsx
+++ src/pages/AboutList.jsx
@@ -1,32 +1,10 @@
-<<<<<<< HEAD
 import AboutCard from "./AboutCard";
 import teamMembers from "../data/teamMembers";
 import "../styles/About.scss";
-function AboutList() {
-  return (
-    <div className="about-background">
-      <div className="about-section">
-        <div className="team-section">
-          {teamMembers.map((teamMember) => (
-            <AboutCard key={teamMember.id} teamMember={teamMember} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-}
 
-export default AboutList;
-=======
-import AboutCard from "./AboutCard";
-import teamMembers from "../data/teamMembers";
-import "../styles/About.scss";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 function AboutList() {
   return (
     <div className="bg-gradient-to-b from-green-50 from-10% via-green-300 via-30% to green-400">
-      <Navbar />
       <div className="about-section">
         <div className="team-section">
           {teamMembers.map((teamMember) => (
@@ -34,10 +12,8 @@ function AboutList() {
           ))}
         </div>
       </div>
-      <Footer />
     </div>
   );
 }
 
 export default AboutList;
->>>>>>> 0a67830f91aa90b1411fd08a7a6db83febdb079f
